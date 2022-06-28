09fd3fb6ea02d7b13adc63bb443f7fb792ea61a7
Madeline <moon.madeline@gmail.com>
2022-06-27 20:16:21 -0500

    Add basic calculator functionality
    Fulfills the following basic functionality requirements as outlined
    in Project: Calculator of The Odin Project:
    
        - Subtracts, adds, divides, and multiplies whole numbers
        - Allows user to clear all history from calculator
        - Evaluates a single pair of numbers at a time
        - Displays an error when a user tries to divide by 0
    
    Future changes will:
    
        - Optimize code for clarity and complexity, simplifying wherever
        possible
        - Limit decimals on the display screen by rounding up
        - Return error if user presses '=' before entering multiple numbers
        - Resolve one pair of numbers at a time and display the working
        math in a smaller, secondary display
        - Allow users to add decimals
        - Add a backspace button so users can modify their entry
        - Add keyboard support

--- calculator.js
+++ calculator.js
@@ -1,60 +1,128 @@
+let displayArray = [];
+let numberArray = [];
+let operatorArray = [];
+let currentDisplay = '';
+const display = document.querySelector('.display');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (button.className === 'digit') {
+      displayNumber(button.name);
+    } else if (button.className === 'operator'){
+      activeOperator(button.name);
+    } else if (button.className === 'equal'){
+      pushToNumberArray();
+      operate(numberArray,operatorArray);
+    } else if (button.name === 'clear') {
+      clearAll();
+    }
+  });
+});
+
+const displayNumber = function(digit) {
+  displayArray.push(digit);
+  currentDisplay = displayArray.join('');
+  display.textContent = `${currentDisplay}`;
+};
+
+const displayResult = function(result) {
+  displayArray = result;
+  numberArray.splice(0, 2, result);
+  display.textContent = `${numberArray[0]}`;
+};
+
+const activeOperator = function(currentOperator) {
+  operatorArray.push(currentOperator);
+  displayArray.push(currentOperator);
+  pushToNumberArray();
+  clearDisplay();
+}
+
+function pushToNumberArray(){
+  numberArray.push(currentDisplay);
+}
+
+function clearDisplay() {
+  displayArray = [];
+  display.textContent = `${currentDisplay}`;
+}
+
+function clearAll(){
+  displayArray = [];
+  numberArray = [];
+  operatorArray = [];
+  currentDisplay = '';
+  display.textContent = '';
+}
+
+function operate(numberArray, operatorArray){
+  clearDisplay();
+  let d = 0;
+  let i = numberArray.length;
+  for (i; i > 0; i--){
+    switch(operatorArray[d]){
+      case '+':
+        displayResult(add(numberArray[0],numberArray[1]));
+        d++;
+        break;
+      case '÷':
+        displayResult(divide(numberArray[0],numberArray[1]));
+        d++;
+        break;
+      case 'x':
+        displayResult(multiply(numberArray[0],numberArray[1]));
+        d++;
+        break;
+      case '-':
+        displayResult(subtract(numberArray[0],numberArray[1]));
+        d++;
+        break;
+      }
+    }
+  }
+
 const add = function(num1, num2) {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
   return num1 + num2; 
 };
 
 const subtract = function(num1, num2) {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
 	return num1 - num2;
 };
 
-const sum = function(array) { 
-  return array.reduce(
-    (total, current) => total + current,
-    0);
+const multiply = function(num1, num2) {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  return num1 * num2;
 };
 
-const multiply = function(array) {
-  return array.length
-    ? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
-};
-
-const divide = function(array) {
-  if (array.includes(0)){
-    return "ERROR"
+const divide = function(num1,num2) {
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  if (num1 === 0 || num2 === 0){
+    return "ERROR";
   } else{
-    return array.length
-    ? array.reduce((accumulator, nextItem) => accumulator / nextItem) : 0;
+    return num1 / num2;
   }
-
-};
-
-const power = function(num,power) {
-  const newNum = num;
-	for (let i = 1; i < power; i++){
-    num = num * newNum;
-  }
-  return num;
-};
-
-const factorial = function(num) {
-  let newNum = 1;
-  for (let i = num; i > 1; i--){
-      newNum = newNum * i;
-  }
-  return newNum;
-};
-
-function operate(num1, num2){
-  add(num1, num2);
 };
 
 
-// // Do not edit below this line
-// module.exports = {
-//   add,
-//   subtract,
-//   sum,
-//   multiply,
-//   power,
-//   factorial,
-//   divide
+// const power = function(num,power) {
+//   const newNum = num;
+// 	for (let i = 1; i < power; i++){
+//     num = num * newNum;
+//   }
+//   return num;
+// };
+
+// const factorial = function(num) {
+//   let newNum = 1;
+//   for (let i = num; i > 1; i--){
+//       newNum = newNum * i;
+//   }
+//   return newNum;
 // };
